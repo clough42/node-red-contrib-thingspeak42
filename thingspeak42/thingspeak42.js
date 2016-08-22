1ea46bd43d8b5fbf4b8ccbd37542d50ded175d98
clough <james.clough@hp.com>
2016-08-22 17:42:14 -0600

    Added icons to config.  Initial experiments with keeping a list.

--- thingspeak42/thingspeak42.js
+++ thingspeak42/thingspeak42.js
@@ -3,11 +3,53 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
 
+        node.delay = config.delay;
+        node.topics = [
+            config.topic1,
+            config.topic2,
+            config.topic3,
+            config.topic4,
+            config.topic5,
+            config.topic6,
+            config.topic7,
+            config.topic8
+            ];
+        node.channel = config.channel;
+        node.apiKey = config.apiKey;
+
+        clearStoredValues();
+
+        function clearStoredValues() {
+            node.values = [ null, null, null, null, null, null, null, null ];
+        };
+
+        function storeValue(index, value) {
+            node.values[index] = value;
+        };
+
+        function getValue(index) {
+            return node.values[i];
+        }
+
         this.on('input', function(msg) {
-            //msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+            for(i=0; i < node.topics.length; i++) {
+                if( msg.topic == node.topics[i] ) {
+                    this.log("Found topic " + i);
+                    storeValue(i, msg.payload);
+                }
+            }
+
+            node.log("Stored values: ");
+            for(i=0; i < node.topics.length; i++) {
+                node.log("  " + i + ": " + getValue(i));
+            }
         });
-    }
+
+        this.on('close', function() {
+
+        });
+    };
+
     RED.nodes.registerType("thingspeak42",ThingSpeak42Node, {
         credentials: {
             channel: {type: "text"},
