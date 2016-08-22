f0c461c87f9b3ff1d4ea7b95c4c5c858ff227b26
clough <james.clough@hp.com>
2016-08-22 11:38:10 -0600

    Add delay config setting.  Change "channelNumber" to "channel" for simplicity.

--- thingspeak42/thingspeak42.js
+++ thingspeak42/thingspeak42.js
@@ -2,6 +2,7 @@ module.exports = function(RED) {
     function ThingSpeak42Node(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+
         this.on('input', function(msg) {
             //msg.payload = msg.payload.toLowerCase();
             node.send(msg);
@@ -9,7 +10,7 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("thingspeak42",ThingSpeak42Node, {
         credentials: {
-            channelNumber: {type: "text"},
+            channel: {type: "text"},
             apiKey: {type: "password"}
         }
     });
